96239678dc967813df1163d0389c05925077413d
Epharra Mendoza <epharrafe@yahoo.com>
2018-07-12 07:33:33 -0500

    First commit

---
+++ src/main/java/com/claim/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.claim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/")
+	public ModelAndView index()
+	{
+		return new ModelAndView("index");
+	}
+	
+}
